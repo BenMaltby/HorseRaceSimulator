0ead98161c7a9c9b8ba126cc023ca68a2d3595cb
rChantx86 <benmaltby@hotmail.co.uk>
2025-04-28 16:11:22 +0100

    Added Horse Class attributes and functionality.

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -2,75 +2,68 @@
 /**
  * Write a description of class Horse here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Benjamin Maltby
+ * @version 22/4/2025
  */
 public class Horse
 {
     //Fields of class Horse
     
-    
+    private String name;
+    private char symbol;
+    private int distanceTravelled;
+    private boolean hasFallen;
+    private double confidence;
       
     //Constructor of class Horse
-    /**
-     * Constructor for objects of class Horse
-     */
-    public Horse(char horseSymbol, String horseName, double horseConfidence)
-    {
-       
+    public Horse(char horseSymbol, String horseName, double horseConfidence) {
+        symbol = horseSymbol;
+        name = horseName;
+        confidence = horseConfidence;
+        distanceTravelled = 0;
+        hasFallen = false;
     }
-    
-    
     
     //Other methods of class Horse
-    public void fall()
-    {
-        
-    }
-    
-    public double getConfidence()
-    {
-        
-    }
-    
-    public int getDistanceTravelled()
-    {
-        
-    }
-    
-    public String getName()
-    {
-        
-    }
-    
-    public char getSymbol()
-    {
-        
-    }
-    
-    public void goBackToStart()
-    {
-        
-    }
-    
-    public boolean hasFallen()
-    {
-        
+    public void fall() {
+        hasFallen = true;
     }
 
-    public void moveForward()
-    {
-        
+    public double getConfidence() {
+        return confidence;
     }
 
-    public void setConfidence(double newConfidence)
-    {
-        
+    public int getDistanceTravelled() {
+        return distanceTravelled;
     }
-    
-    public void setSymbol(char newSymbol)
-    {
-        
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public void goBackToStart() {
+        distanceTravelled = 0;
+        hasFallen = false;
+    }
+
+    public boolean hasFallen() {
+        return hasFallen;
+    }
+
+    public void moveForward() {
+        distanceTravelled++;
+    }
+
+    public void setConfidence(double newConfidence) {
+        confidence = newConfidence;
+    }
+
+    public void setSymbol(char newSymbol) {
+        symbol = newSymbol;
     }
     
 }
